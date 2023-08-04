c984d0406a03914530206c00a5afdd401c1b0f2d
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2023-08-04 08:42:28 +0530

    Added adduser activity

---
+++ app/src/main/java/com/example/monkhoodassignment/AddUsers.kt
@@ -0,0 +1,11 @@
+package com.example.monkhoodassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddUsers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_users)
+    }
+}
