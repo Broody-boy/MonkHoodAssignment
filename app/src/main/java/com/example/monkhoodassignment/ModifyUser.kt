8c467c81d8d48906d32158dcf773858788189359
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2023-08-04 08:25:30 +0530

    Renamed activity AddModifyUser to ModifyUser

--- app/src/main/java/com/example/monkhoodassignment/ModifyUser.kt
+++ app/src/main/java/com/example/monkhoodassignment/ModifyUser.kt
@@ -3,9 +3,9 @@ package com.example.monkhoodassignment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AddModifyUser : AppCompatActivity() {
+class ModifyUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_modify_user)
+        setContentView(R.layout.activity_modify_user)
     }
 }
